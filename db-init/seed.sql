e13caf23dcb18154e58246e378975958e17b8478
brandon.salvietti <brandon.salvietti@mymail.champlain.edu>
2026-04-06 15:01:20 -0400

    changed some seed data

--- db-init/seed.sql
+++ db-init/seed.sql
@@ -6,24 +6,15 @@ CREATE TABLE IF NOT EXISTS users (
     deleted_at TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS special_chud(
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    description_of_chud TEXT
-);
-
 INSERT INTO users (user_name, email) VALUES
     ('hoose', 'smoose@huh.com'),
     ('vg3o', 'bruh@email.com'),
     ('holdin_mcgroin', 'email@connor.com'),
     ('bigManSees', 'newguy@email.com'),
     ('hasbro', 'feeder@deadlock.com'),
-    ('kai', 'farts@strigid.co'),
-    ('xepois', 'stupid@strigid.co'),
+    ('kai', 'kai@strigid.co'),
+    ('xepois', 'british@strigid.co'),
     ('jeffery cheeseburger', 'jeffbogo@strigid.co'),
     ('mylo', 'fish@mail.com'),
     ('fifth guy burger', 'burger@mail.com')
 ON CONFLICT (email) DO NOTHING;
-
-INSERT INTO special_chud (user_id, description_of_chud) VALUES
-    (5, 'Love you pwetril');
